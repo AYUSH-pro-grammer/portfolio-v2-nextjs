faccdb015cb01b65db8c3dc2db34432edd8c6746
Ayush Kumar <167435819+AYUSH-pro-grammer@users.noreply.github.com>
2026-05-03 16:11:28 +0530

    navbar added

---
+++ component/navbar/Navbar.tsx
@@ -0,0 +1,239 @@
+'use client';
+import styles from "./Navbar.module.css"
+import Lottie, {type LottieRefCurrentProps} from "lottie-react"
+import { useState, useRef, useEffect} from "react";
+
+import nav from "@/assets/json/nav.json"
+import arrowNav from "@/assets/json/arrowNav.json"
+import Scan from "@/assets/json/Scan.json"
+
+
+interface NavbarProps{
+    no: number
+}
+
+export default function Navbar({no}: NavbarProps){
+
+    const lottieref1 = useRef<LottieRefCurrentProps | null>(null);
+    const lottieref2 = useRef<LottieRefCurrentProps | null>(null);
+    const lottieref3 = useRef<LottieRefCurrentProps | null>(null);
+    const lottieref4 = useRef<LottieRefCurrentProps | null>(null);
+    const userIconRef = useRef<LottieRefCurrentProps | null>(null);
+
+    const [active, set_active] = useState(0);
+
+    function changeActive(no: number){
+        set_active(no)
+    }
+
+
+    return (
+        <div className={styles.NavbarcONT}> 
+            
+            <div className={styles.OneMore}>
+
+
+<div>
+
+
+                <div className={styles.upperbodyName}>
+
+                    <div
+                    
+                    onMouseEnter={()=>{userIconRef.current?.play()}}
+                    onMouseLeave={()=>{userIconRef.current?.stop()}}
+                    className={styles.navbarScan}
+                    >
+
+                    <Lottie
+
+                    lottieRef={userIconRef}
+                    animationData={Scan}
+                    loop={false}
+                    autoplay={false}
+                    className={styles.lottieScan}
+                    
+                    
+                    >
+
+                    </Lottie>
+
+                    </div>
+
+
+                    <div className={styles.upperbodyNametEXT}>
+                         <h4>Ayush Kumar</h4>
+                        <h6>Software Dev</h6>
+                    </div>
+                   
+                </div>
+
+
+<br />
+
+                <ul className={styles.uperlowerNav}>
+                    <li onClick={()=>{changeActive(0)}}
+                    className={active === 0 ? styles.active : styles.notActive}
+                        
+                        >Home
+
+                        {active === 0 ? (
+                        <div className={styles.dot}>·</div>
+
+                        ):(
+                            <p className={styles.none}></p>
+                        )}
+
+                        
+                        
+                        </li>
+                    <li onClick={()=>{changeActive(1)}}
+                    className={active === 1 ? styles.active : styles.notActive}
+                        >About
+                        
+                        {active === 1 ? (
+                        <div className={styles.dot}>·</div>
+
+                        ):(
+                            <p className={styles.none}></p>
+                        )}
+                        
+                        </li>
+                    <li onClick={()=>{changeActive(2)}}
+                    className={active === 2 ? styles.active : styles.notActive}
+                        >Project
+                        
+                        {active === 2 ? (
+                        <div className={styles.dot}>·</div>
+
+                        ):(
+                            <p className={styles.none}></p>
+                        )}
+                        
+                        </li>
+                    <li onClick={()=>{changeActive(3)}}
+                    className={active === 3 ? styles.active : styles.notActive}
+                        >Stack
+                        
+                        {active === 3 ? (
+                        <div className={styles.dot}>·</div>
+
+                        ):(
+                            <p className={styles.none}></p>
+                        )}
+                        
+                        </li>
+                    <li onClick={()=>{changeActive(4)}}
+                    className={active === 4 ? styles.active : styles.notActive}
+                        >Contact
+                        
+                        {active === 4 ? (
+                        <div className={styles.dot}>·</div>
+
+                        ):(
+                            <p className={styles.none}></p>
+                        )}
+                        
+                        </li>
+                </ul>
+
+
+
+</div>
+
+
+
+    <ul className={styles.lowerMainPart}>
+        <li
+        
+        
+                      onMouseEnter={()=>{lottieref1.current?.play()}}
+                      onMouseLeave={()=>{lottieref1.current?.stop()}}
+
+>
+            <h6>Email</h6>
+
+            <div className={styles.lowerScaleChange}
+            >
+                <Lottie
+
+                lottieRef={lottieref1}
+                animationData={arrowNav}
+                loop={true}      
+                className={styles.littieBounseArrow}
+                autoplay={false}
+                >
+                    
+                </Lottie>
+
+
+            </div>
+        </li>
+        <li
+        
+                      onMouseEnter={()=>{lottieref2.current?.play()}}
+                      onMouseLeave={()=>{lottieref2.current?.stop()}}
+
+>
+                        <h6>Github</h6>
+
+            <div className={styles.lowerScaleChange}
+            
+
+>
+                <Lottie
+
+                lottieRef={lottieref2}
+                animationData={arrowNav}
+                loop={true}      
+                className={styles.littieBounseArrow}
+                autoplay={false}
+
+                >
+                    
+                </Lottie>
+
+                
+            </div>
+
+        </li>
+        <li
+        
+                      onMouseEnter={()=>{lottieref3.current?.play()}}
+                      onMouseLeave={()=>{lottieref3.current?.stop()}}
+
+>
+                        <h6>Linkedin</h6>
+
+            <div className={styles.lowerScaleChange}
+            
+                    
+                    >
+                <Lottie
+                lottieRef={lottieref3}
+                animationData={arrowNav}
+                loop={true}      
+                className={styles.littieBounseArrow}
+                autoplay={false}
+
+                >
+                    
+                </Lottie>
+
+                
+            </div>
+        </li>
+    </ul>
+
+
+
+
+
+
+
+
+
+            </div>
+        </div>
+    )
+}
