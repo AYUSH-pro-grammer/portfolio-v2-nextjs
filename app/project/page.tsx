0ad486062a64cd2d205ff47a5f84cbbf41a6a008
Ayush Kumar <ayushpro1428@gmail.com>
2026-08-03 13:33:41 +0530

    reframe projects page

--- app/project/page.tsx
+++ app/project/page.tsx
@@ -10,54 +10,207 @@ import keyboard from "@/public/projectImage/keyboard.png"
 import drone from "@/public/projectImage/drone.png"
 
 
+import blinkyboard from "@/public/projectImage/blinkyboard.png"
+import hermes from "@/public/projectImage/hermes.png"
+import keycaps from "@/public/projectImage/keycaps.png"
+import pathfinder from "@/public/projectImage/pathfinder.png" 
+import rccar from "@/public/projectImage/rccar.png"
+
+import robodog from "@/public/projectImage/robodog.png" 
+import splitkeyboard from "@/public/projectImage/splitkeyboard.png"
+import flightcontroller from "@/public/projectImage/flightcontroller.png"
+
+import hackpad from "@/public/projectImage/hackpad.png"     
+import { useState } from "react"
+
 
 
 export default function ProjectPage(){
 
-    const data = [
+
+    const [active, setActive] = useState(1)
+
+    const changeActive = (index: number) => {
+        setActive(index)
+    }
+
+    const completed_data = [
         {
             "title" : "Custom Keybaord",
             "github_url": "https://github.com/AYUSH-pro-grammer/keyboard",
-            "image":keyboard
+            "image":keyboard,
+             "demo": "",
+            "guide": ""
 
         },
 
         {
             "title": "Devboard",
             "github_url": "https://github.com/AYUSH-pro-grammer/devboard",
-            "image": devboard
+            "image": devboard,
+             "demo": "",
+            "guide": "",
         },
         {
             "title": "Drone",
             "github_url": "",
-            "image": drone
+            "image": drone,
+             "demo": "",
+            "guide": "",
 
+        },
+        {
+
+            "title" : "Hackpad",
+            "github_url": "",
+            "image": hackpad,
+             "demo": "",
+            "guide": "",
+        }
+
+    ]
+
+
+
+    const pending_data = [
+        {
+            "title" : "Split Keyboard",
+            "github_url": "https://github.com/AYUSH-pro-grammer/split-keyboard",
+            "image":splitkeyboard,
+            "demo": "",
+            "guide": ""
+
+        },
+
+        {
+
+            "title": "blinky timer board",
+            "github_url": "https://github.com/AYUSH-pro-grammer/led-timer",
+            "image": blinkyboard,
+            "demo": "",
+            "guide": "",
+            
+        },
+
+        {
+            "title": "Hermes",
+            "github_url": "https://github.com/AYUSH-pro-grammer/hermes",
+            "image": hermes,
+            "demo": "",
+            "guide": "",
+        },
+        {
+            "title": "Flight Controller",
+            "github_url": "https://github.com/AYUSH-pro-grammer/esp32-flight-controller",
+            "image": flightcontroller,
+        },
+        {
+            "title": "Keycaps", 
+            "github_url": "https://github.com/AYUSH-pro-grammer/keycaps",
+            "image": keycaps,
+            "demo": "",
+            "guide": "",
+        }, {
+
+
+            "title": "Path Finder",
+            "github_url": "https://github.com/AYUSH-pro-grammer/pathFinder",
+            "image" : pathfinder,
+            "demo": "",
+            "guide": "",
+        },
+        {
+            "title": "RC Car",
+            "github_url": "https://github.com/AYUSH-pro-grammer/rc-camera-car",
+            "image": rccar,
+            "demo": "",
+            "guide": "",
+        },
+        {
+            "title": "Robo Dog",
+            "github_url": "https://github.com/AYUSH-pro-grammer/robo-dog-esp32",
+            "image": robodog,
+            "demo": "",
+            "guide": "",
         }
     ]
+
+
+
+
+
+
     return (
         <div>
 
+
+
             <aside>
                 <h1>Project</h1>
+
+                <div className={styles.filter} >
+                    <button className={active === 1 ? styles.selected: styles.filterbutton}
+
+                        onClick={()=> changeActive(1)}
+                    >Completed</button>
+
+
+                    <button className={active === 2 ? styles.selected: styles.filterbutton}
+
+                    onClick = {() => changeActive(2)}
+                    
+
+                    >In Progress</button>
+                </div>
+
+                <br/>
+
+{
+    active === 1 ? (
+
 
                 <div className={styles.whitebox}>
 
                     {
 
-                        data.map((item: any) => {
+                        completed_data.map((item: any) => {
                             return (
                                 <div className={styles.box}> 
                                     <Image className={styles.boximage}  src={item.image} alt="project image"></Image>
                                     <h4>{item.title}</h4>
 
+
+                                    <div className={styles.linkBox}>
+
                                     {
+
                                         item.github_url ? (
-                                            <a href={item.github_url}>Link</a>
+                                            <a href={item.github_url}>Github</a>
                                         ):(
                                             <h6>Comming Soon
                                             </h6>
                                         )
                                     }
+
+
+                                    {
+
+                                        item.demo ? (
+                                            <a href={item.demo}>Demo</a>
+                                        ): ""
+                                    }
+
+                                    {
+                                        item.guide ? (
+                                            <a href={item.guide}>Guide</a>
+                                        ): ""
+                                    }
+
+
+
+                                    </div>
+
+                                   
                                 </div>
                             )
                         })
@@ -65,6 +218,61 @@ export default function ProjectPage(){
                     }
 
                 </div>
+    ):(
+
+   <div className={styles.whitebox}>
+
+                    {
+
+                        pending_data.map((item: any) => {
+                            return (
+                                <div className={styles.box}> 
+                                    <Image className={styles.boximage}  src={item.image} alt="project image"></Image>
+                                    <h4>{item.title}</h4>
+
+
+                                    <div className={styles.linkBox}>
+
+                                    {
+
+                                        item.github_url ? (
+                                            <a href={item.github_url}>Github</a>
+                                        ):(
+                                            <h6>Comming Soon
+                                            </h6>
+                                        )
+                                    }
+
+
+                                    {
+
+                                        item.demo ? (
+                                            <a href={item.demo}>Demo</a>
+                                        ): ""
+                                    }
+
+                                    {
+                                        item.guide ? (
+                                            <a href={item.guide}>Guide</a>
+                                        ): ""
+                                    }
+
+
+
+                                    </div>
+
+                                   
+                                </div>
+                            )
+                        })
+
+                    }
+
+                </div>
+    )
+}
+
+
             </aside>
 
             <Stack/>
